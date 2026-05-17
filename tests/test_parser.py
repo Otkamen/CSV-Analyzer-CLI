44eb1d09d87fa0f954d6e6d8876a3fe829ecf1a0
Otkaman <ucynartem@gmail.com>
2026-05-17 17:52:31 +0300

    re-write test_parser with path module

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,21 +1,31 @@
 import pytest
 from parser import readFile
-from exceptions import ParseError
+from exceptions import ParseError, ColumnNotFoundError
 
 
 def test_file_not_found():
     with pytest.raises(FileNotFoundError):
         readFile('File doesn`t exist.csv')
 
-def test_wrong_extension():
+
+def test_wrong_extension(tmp_path):
+    test_file = tmp_path / "test.txt"
+    test_file.write_text("num,category\n1,2\n")
+    
     with pytest.raises(ParseError):
-        readFile('../report.txt')
-
-def test_returns_list():
-    assert readFile('./tests/tests_data/file_exist.csv') == ['3','7','6']
+        readFile(str(test_file))
 
 
+def test_wrong_column(tmp_path):
+    test_file = tmp_path / "file_exist.csv"
+    test_file.write_text("num,num,category\n1,2,3\n2,3,7\n3,5,6\n")
+    
+    with pytest.raises(ColumnNotFoundError):
+        readFile(str(test_file), column='price')
 
 
-
-
+def test_returns_list(tmp_path):
+    test_file = tmp_path / "file_exist.csv"
+    test_file.write_text("num,num,category\n1,2,3\n2,3,7\n3,5,6\n")
+    
+    assert readFile(str(test_file)) == ['3', '7', '6']
